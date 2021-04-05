fe15f1ce95f67c051c0f71d2ba9982126a7dd688
eric <kvrcode98@gmail.com>
2021-04-05 15:11:08 -0400

    cleaning up code

--- calculator/app.js
+++ calculator/app.js
@@ -1,14 +1,12 @@
-// need to update total value on screen as user chains together operations
 // need to convert large numbers exceeding length 15 to 10e15 format
 //above already happens but theres a grey area that would ideally be taken care of i think 10e15 to 10e20
 //need to add commas to numbers > 999
-//need to add support for negative numbers
 
 //calculator "state" object
 let calculator = {
     total: '',
     running_total: '',
-    current_int: '',
+    current_int: '0',
     operation: ''
 }
 
@@ -26,6 +24,15 @@ function divide(x, y) {
     return x / y;
 }
 
+//resets state object values to default and resets screen
+const handle_clear = (callback) => {
+    calculator.total = '';
+    calculator.running_total = '';
+    calculator.current_int = '0';
+    calculator.operation = '';
+    callback(calculator.current_int);
+}
+
 // returns boolean to run as a check against returned results
 const divide_by_zero = () => {
     if(calculator.operation === 'divide' && calculator.current_int === '0') {
@@ -35,21 +42,11 @@ const divide_by_zero = () => {
     }
 }
 
-//clears state object and resets screen
-const handle_clear = () => {
-    calculator.total = '';
-    calculator.running_total = '';
-    calculator.current_int = '';
-    calculator.operation = '';
-    update_screen('');
-    console.log(calculator);
-}
-
 //handles evaluation via callback
 //handles returned result appropriately
 const operate = (callback, x, y) => {
     let result = window[callback](x, y); //easier callback method mentioned above...at least i believe this is easier way
-    if(result % 1 !== 0 ) { result.toFixed(4).toString();}
+    if(result % 1 !== 0 ) { result = result.toFixed(4) }
     calculator.running_total = result.toString();
 
     if(divide_by_zero()) { calculator.running_total = 'ask siri...'};
@@ -58,14 +55,58 @@ const operate = (callback, x, y) => {
 }
 
 const number_callback = (number) => {
-    if(number.textContent === '.' && calculator.current_int.includes('.')) return //82.201.3.4 eh
-    // if(number.textContent === '-' && calculator.current_int.includes('-')) return
+    if(number.textContent === '.' && calculator.current_int.includes('.')) return //82.201.3.4 not trying to multiply ip addresses
+    if(number.textContent === '0' && calculator.current_int === '0') return //doesnt allow '00...'
+    
+    if(calculator.current_int === '0' && number.textContent === '.' ) { //allows decimals to appear as 0.5, or et cetera
+        calculator.current_int += number.textContent;
+    } else if (calculator.current_int === '0') { //when entering integers, replaces '0' placeholder
+        calculator.current_int = number.textContent;
+    } else { //normal behavior, "copies" entered numbers as expected
+        calculator.current_int += number.textContent;
+    }
+    if(calculator.current_int.length > 15) { return } //limits length of numbers
 
-    calculator.current_int += number.textContent;
-    if(calculator.current_int.length > 15) { return }
-
-    console.log(calculator);
     update_screen(calculator.current_int);
+}
+
+const operator_callback = (operator) => {
+    if(calculator.current_int !== '' && calculator.running_total !== '') { //allows chaining
+        operate(calculator.operation, parseFloat(calculator.running_total), parseFloat(calculator.current_int))
+    }
+
+    calculator.operation = operator.value;
+
+    if(calculator.running_total === '') {calculator.running_total = calculator.current_int;}
+    calculator.current_int = '';
+
+    update_screen(calculator.running_total); //useful when chaining to show your total as you continue
+    // update_screen(''); //just show a blank screen until you press enter..?
+}
+
+const enter_callback = () => {
+    if(calculator.running_total === '' || calculator.current_int === '') return //can't evaluate a number and an empty string - "no number" - together
+
+    const previous = calculator.running_total;
+    const current = calculator.current_int;
+    const operation = calculator.operation;
+
+    operate(operation, parseFloat(previous), parseFloat(current));
+
+    calculator.current_int = ''; //resets current int to blank, if chaining, running total is stored in calculator.running_total
+    calculator.operation = ''; //resets operation to default
+}
+
+const toggle_negative_callback = () => {
+    if(calculator.current_int === '0') return //no -0
+
+    if (!calculator.current_int.includes('-')) { //if not negative, add negative sign
+        calculator.current_int = '-' + calculator.current_int;
+        update_screen(calculator.current_int);
+    } else if (calculator.current_int.split('')[0] === '-') { //if negative, remove negative sign
+        calculator.current_int = calculator.current_int.substring(1);
+        update_screen(calculator.current_int);
+    }
 }
 
 //handles number keypresses and displaying to user
@@ -82,50 +123,31 @@ handle_number_event();
 const handle_operator_event = () => {
     const operators = document.querySelectorAll('.operator');
 
-    let old_operator = '';
-
     operators.forEach(operator => {
-        operator.addEventListener('click', () => {
-
-            if(calculator.current_int !== '' && calculator.running_total !== '') { //allows chaining
-                operate(calculator.operation, parseFloat(calculator.running_total), parseFloat(calculator.current_int))
-            }
-
-            if(calculator.running_total !== '' ) { old_operator = operator.value; }
-            // console.log(old_operator)
-
-            calculator.operation = operator.value;
-
-            if(calculator.running_total === '') {calculator.running_total = calculator.current_int;}
-            calculator.current_int = '';
-
-            if(calculator.current_int === '' && operator.value === 'subtract' && calculator.running_total === '') { //lets you place a negative before first operand
-                // console.log('neg');
-                calculator.current_int += '-';
-                // update_screen('-');
-                update_screen(calculator.current_int);
-                console.log(calculator);
-            } else if (calculator.current_int.includes('-') && operator.value === 'subtract'){ //if already a negative number, just allows you to subtract
-                calculator.operation = 'subtract';
-                console.log('neg #');
-            } else if (calculator.running_total !== '' && operator.value === 'subtract' && old_operator !== 'subtract'){ //makes 2nd operand a negative number without messing up previously selected operator
-                calculator.current_int += '-';
-                // calculator.operation = old_operator;
-                old_operator = '';
-                // calculator.value = operation;
-            }
-
-            update_screen('');
-        })
+        operator.addEventListener('click', () => operator_callback(operator));
     })
 }
 handle_operator_event();
+
+//handles enter key press and logic to follow
+const handle_enter = () => {
+    const enter = document.querySelector('.enter');
+    enter.addEventListener('click', enter_callback);
+}
+handle_enter();
+
+
+const toggle_negative_number = () => {
+    const negative = document.querySelector('.negative');
+    negative.addEventListener('click', toggle_negative_callback);
+}
+toggle_negative_number();
 
 //handles clear button keypress
 const clear = () => {
     const clear_btn = document.querySelector('.clear');
     clear_btn.addEventListener('click', () => {
-        handle_clear();
+        handle_clear(update_screen);
     });
 }
 clear();
@@ -138,23 +160,6 @@ const update_screen = (value) => {
     screen.innerHTML = str;
 }
 
-//handles enter key press and logic to follow
-const handle_enter = () => {
-    const enter = document.querySelector('.enter');
-    enter.addEventListener('click', () => { //this seems too messy
-        if(calculator.running_total === '' || calculator.current_int === '') return //can't evaluate a number and an empty string - "no number" - together
-        const previous = calculator.running_total;
-        const current = calculator.current_int;
-        const operation = calculator.operation;
-
-        operate(operation, parseFloat(previous), parseFloat(current));
-
-        calculator.current_int = '';
-        calculator.operation = '';
-    })
-}
-handle_enter();
-
 const undo = () => {
     document.addEventListener('keydown', (e) => {
         if(e.key === 'Backspace') {
@@ -166,31 +171,6 @@ const undo = () => {
 }
 undo();
 
-const toggle_negative_number = () => {
-    const negative = document.querySelector('.negative');
-    // const subtract = document.querySelector('.subtract');
-
-    negative.addEventListener('click', () => {
-
-        // if (!calculator.current_int.includes('-')) return
-        if (!calculator.current_int.includes('-')) {
-            // calculator.current_int.unshift('-');
-            calculator.current_int = '-' + calculator.current_int;
-            update_screen(calculator.current_int);
-            console.log(calculator);
-            // calculator.current_int.unshift('-'); //just to explicitly place at beginning
-        } else if (calculator.current_int.split('')[0] === '-') {
-            // calculator.current_int.shift();
-            calculator.current_int = calculator.current_int.substring(1);
-            console.log(calculator);
-            update_screen(calculator.current_int);
-        }
-
-    })
-}
-toggle_negative_number();
-
-//absolutely nothing right now
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('start');
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    update_screen(calculator.current_int);
+})
